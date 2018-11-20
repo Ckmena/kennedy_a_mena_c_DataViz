0557919948e9dceb5c690e0f457e540e50ff4cd9
Ckmena <c_menavalencia@fanshaweonline.ca>
2018-11-20 17:23:32 -0500

    just to read the csv file

--- dataviz_kennedy_mena.py
+++ dataviz_kennedy_mena.py
@@ -7,31 +7,40 @@ gender= [] # push the installs data here
 medals = [] #push the ratings data here
 
 with open ("data/Olympics-Winter.csv") as csvfile:
-    reader = csv.reader(csvfile)
+    readerCSV = csv.reader(csvfile)
     line_count = 0
     #print('Works')
 
-    for row in reader:
-        if line_count is 0: # strip the header out
-            print('pushing text row to categories array')
-            gender.append(row)
-            line_count += 1
+    for row in readerCSV:
+        gender = row[5] 
+        medals = row[7]
 
-        else:
-            print("collect the rest of the data")
-            genderData = (row[2])
-            gender.append(float(genderData))
+        gender.append(gender)
+        medals.append(medals)
+
+        print(gender)
+        print(medals)
+        
+        #if line_count is 0: # strip the header out
+            #print('pushing text row to categories array')
+            #gender.append(row)
+            #line_count += 1
+
+        #else:
+            #print("collect the rest of the data")
+            #genderData = (row[2])
+            #gender.append(float(genderData))
             
 
             #print('collect the rest of the data') 
-            medalData = row[5]
-            medalData = medalData.replace(",", "")
-            medalData = medalData.replace("Free", "0")
-            medals.append(int(np.char.strip(medalData, "+")))
-            line_count += 1
+            #medalData = row[5]
+            #medalData = medalData.replace(",", "")
+            #medalData = medalData.replace("Free", "0")
+            #medals.append(int(np.char.strip(medalData, "+")))
+            #line_count += 1
 
-print('processed', line_count, "rows of data")
-print('first line', gender[0])
-print('last line', gender[-1])
+#print('processed', line_count, "rows of data")
+#print('first line', gender[0])
+#print('last line', gender[-1])
 
-np_gender = np.array(gender)
+#np_gender = np.array(gender)
