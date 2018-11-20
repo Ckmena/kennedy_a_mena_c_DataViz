d66eb0f5c36d809ba9d53f1837d3df2e6b7b4e4c
Ckmena <c_menavalencia@fanshaweonline.ca>
2018-11-20 16:48:55 -0500

    organizing files- make .py to work with new .csv file

---
+++ dataviz_kennedy_mena.py
@@ -0,0 +1,37 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+year = [] # strip out the first row of text
+gender= [] # push the installs data here
+medals = [] #push the ratings data here
+
+with open ("data/Olympics-Winter.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    line_count = 0
+    #print('Works')
+
+    for row in reader:
+        if line_count is 0: # strip the header out
+            print('pushing text row to categories array')
+            gender.append(row)
+            line_count += 1
+
+        else:
+            print("collect the rest of the data")
+            genderData = (row[2])
+            gender.append(float(genderData))
+            
+
+            #print('collect the rest of the data') 
+            medalData = row[5]
+            medalData = medalData.replace(",", "")
+            medalData = medalData.replace("Free", "0")
+            medals.append(int(np.char.strip(medalData, "+")))
+            line_count += 1
+
+print('processed', line_count, "rows of data")
+print('first line', gender[0])
+print('last line', gender[-1])
+
+np_gender = np.array(gender)
